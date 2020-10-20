73d0b1aafb100e5cd68cd0ff08f6096d90729cfc
mirrorhanyu <hyu@thoughtworks.com>
2020-10-20 22:52:00 +0800

    fix download file path

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ feeds_xml = requests.get(youtube_feeds_url).text
 for entry in YoutubeFeed(feeds_xml).entries[:1]:
     description = entry.media_description[:250]
     with youtube_dl.YoutubeDL({
-        'outtmpl': entry.title
+        'outtmpl': 'youtube-download-file'
     }) as ydl:
         ydl.download([f'https://www.youtube.com/watch?v={entry.video_id}'])
         title = f'[Bora] {entry.title}'
         entertainment_video_type = 71
         tags = ['颜值', 'YOUTUBE搬运', '美女', '韩国', '时尚穿搭', '旅行']
         source = 'http://www.youtube.com'
-        filepath = glob.glob(f'{entry.title}')[0]
+        filepath = glob.glob(f'{filepath}')[0]
         bilibili = Bilibili(os.getenv('BILIBILI_COOKIE', ''))
         bilibili.upload(
             parts=[
