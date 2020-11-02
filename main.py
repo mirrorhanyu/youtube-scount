7ea0dca30ae0fbc939a60776299ca4c113d477a5
mirrorhanyu <mirrorhanyu@gmail.com>
2020-11-02 22:16:10 +0800

    fix next iteration again

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ saved_youtube_ids = [saved_youtube.get('id') for saved_youtube in db.find_all('s
 youtube_feeds_url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCuYtwhBPiVI5UiZgT3GEdAw'
 feeds_xml = requests.get(youtube_feeds_url).text
 
-entry = next((entry for entry in YoutubeFeed(feeds_xml).entries if entry.video_id not in saved_youtube_ids), default=None)
+entry = next((entry for entry in YoutubeFeed(feeds_xml).entries if entry.video_id not in saved_youtube_ids), None)
 
 if entry is not None:
     description = entry.media_description[:250]
